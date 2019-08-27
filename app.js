5e29e3c3f82f30166cd2553519f2a7d74f489559
sam gaudet <samgaudet@samsMacmini.telus>
2019-08-27 16:07:08 -0600

    - added functions to load/delete invitees from database - added cors node module

--- app.js
+++ app.js
@@ -21,31 +21,80 @@ document.addEventListener('DOMContentLoaded', () => {
   // 1. Create a new XMLHttpRequest object
   let xhr = new XMLHttpRequest();
 
-  // 2. Configure it: GET-request for the URL /article/.../load
-  xhr.open('GET', '/localhost:3000/invitees');
+  loadInviteesAsync();
 
-  // 3. Send the request over the network
-  xhr.send();
+  function loadInviteesAsync() {
 
-  filterCheckBox.addEventListener('change', (e) => {
-    const isChecked = e.target.checked;
-    const lis = ul.children;
-    for(let i = 0; i < lis.length; i++) {
-      let li = lis[i]
-      if(isChecked === true) {
-          if(li.className !== 'responded') {
-               li.style.display = 'none';
-          } else {
-             li.style.display = ''
-          }
-      } else {
-        li.style.display = ''
+    // 2. Configure it: GET-request for the URL /article/.../load
+    xhr.open('GET', 'http://localhost:3000/invitees');
+
+    // 3. Send the request over the network
+    xhr.send();
+
+    // 4. This will be called after the response is received
+    xhr.onload = function() {
+      if (xhr.status != 200) { // analyze HTTP status of the response
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+      } else { // show the result
+        //alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+        var invitees = JSON.parse(xhr.response);
+        for(var i = 0; i < invitees.length; i++) {
+          // console.log(invitees[i]);
+          // console.log(invitees[i].name);
+          const invitee = invitees[i];
+          const li = loadInvitee(invitee.name, invitee.confirmed);
+
+          ul.appendChild(li);
+        }
       }
+    };
+  }
+
+  function removeInviteeAsync(name) {
+    xhr.open('DELETE', `http://localhost:3000/invitees/${name}`);
+
+    xhr.send();
+
+    xhr.onload = function() {
+      if (xhr.status != 200) { // analyze HTTP status of the response
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+      } else { // show the result
+        alert(`Removed invitee: ${name}`);
+      };
+    }
+  }
+
+  function loadInvitee(name, confirmed) {
+    //console.log('createLI', text);
+
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
     }
 
-  });
+    function appendToLi(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
+    }
+
+    const li = document.createElement('li');
+    const checkBox = createElement('input', 'type', 'checkBox');
+
+    checkBox.checked = confirmed;
+
+    appendToLi('span', 'textContent', name);
+    appendToLi('label', 'textContent', 'Confirmed')
+    .appendChild(checkBox);
+    appendToLi('button', 'textContent', 'edit');
+    appendToLi('button', 'textContent', 'remove');
+
+    return(li);
+  }
 
   function createLI(text) {
+    console.log('createLI', text);
 
     function createElement(elementName, property, value) {
       const element = document.createElement(elementName);
@@ -69,6 +118,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     return(li);
   }
+
+  filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    for(let i = 0; i < lis.length; i++) {
+      let li = lis[i]
+      if(isChecked === true) {
+          if(li.className !== 'responded') {
+               li.style.display = 'none';
+          } else {
+             li.style.display = ''
+          }
+      } else {
+        li.style.display = ''
+      }
+    }
+  });
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -98,6 +164,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const action = button.textContent;
       const nameActions = {
         remove:  () => {
+          const spanText = li.firstElementChild.textContent;
+          removeInviteeAsync(spanText);
           ul.removeChild(li);
         },
         edit: () => {
@@ -120,7 +188,6 @@ document.addEventListener('DOMContentLoaded', () => {
       };
 
       nameActions[action]();
-
     }
   });
 });
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const queries = require('./queries');
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
